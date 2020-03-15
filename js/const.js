dceb03aa8c48c3a1d6594d803a590af4308e70d8
el ganso <elganso45@gmail.com>
2020-03-15 15:13:50 +0300

    не получается показать магов

--- js/const.js
+++ js/const.js
@@ -9,7 +9,6 @@
   var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
   var FIRE_BALL_COLOR = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
-
   window.const = {
     escKey: ESC_KEY,
     enterKey: ENTER_KEY,
--- js/setup.js
+++ js/setup.js
@@ -1,49 +1,96 @@
 'use strict';
+(function () {
+  // var userDialog = document.querySelector('.setup');
 
-var userDialog = document.querySelector('.setup');
+  document.querySelector('.setup-similar').classList.remove('hidden');
 
-document.querySelector('.setup-similar').classList.remove('hidden');
-
-var similarListElement = document.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+  // var similarListElement = document.querySelector('.setup-similar-list');
+  // var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
 
-var wizards = [
-  {
-    name: window.const.wizardsNames[Math.floor(Math.random() * window.const.wizardsNames.length)] + '\r\n' + window.const.wizardsSurename[Math.floor(Math.random() * window.const.wizardsSurename.length)],
-    coatColor: window.const.wizardsCoatColor[Math.floor(Math.random() * window.const.wizardsCoatColor.length)],
-    eyesColor: window.const.WizardsEyesColor[Math.floor(Math.random() * window.const.WizardsEyesColor.length)]
-  }
-];
+  // var wizards = [
+  //   {
+  //     name: window.const.wizardsNames[Math.floor(Math.random() * window.const.wizardsNames.length)] + '\r\n' + window.const.wizardsSurename[Math.floor(Math.random() * window.const.wizardsSurename.length)],
+  //     coatColor: window.const.wizardsCoatColor[Math.floor(Math.random() * window.const.wizardsCoatColor.length)],
+  //     eyesColor: window.const.WizardsEyesColor[Math.floor(Math.random() * window.const.WizardsEyesColor.length)]
+  //   }
+  // ];
 
-var renderWizard = function (wizard) {
-  var wizardElement = similarWizardTemplate.cloneNode(true);
+  // var renderWizard = function (wizard) {
+  //   var wizardElement = similarWizardTemplate.cloneNode(true);
 
-  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  //   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  //   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  //   wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
 
-  return wizardElement;
-};
+  //   return wizardElement;
+  // };
 
-function createRandomWizard() {
-  return {
-    name: window.const.wizardsNames[Math.floor(Math.random() * window.const.wizardsNames.length)] + '\r\n' + window.const.wizardsSurename[Math.floor(Math.random() * window.const.wizardsSurename.length)],
-    coatColor: window.const.wizardsCoatColor[Math.floor(Math.random() * window.const.wizardsCoatColor.length)],
-    eyesColor: window.const.WizardsEyesColor[Math.floor(Math.random() * window.const.WizardsEyesColor.length)]
+  // function createRandomWizard() {
+  //   return {
+  //     name: window.const.wizardsNames[Math.floor(Math.random() * window.const.wizardsNames.length)] + '\r\n' + window.const.wizardsSurename[Math.floor(Math.random() * window.const.wizardsSurename.length)],
+  //     coatColor: window.const.wizardsCoatColor[Math.floor(Math.random() * window.const.wizardsCoatColor.length)],
+  //     eyesColor: window.const.WizardsEyesColor[Math.floor(Math.random() * window.const.WizardsEyesColor.length)]
+  //   };
+  // }
+
+  // for (var i = 0; i < 3; i++) {
+  //   var wizard = createRandomWizard();
+  //   wizards.push(wizard);
+  // }
+
+  // var fragment = document.createDocumentFragment();
+  // for (i = 0; i < wizards.length; i++) {
+  //   fragment.appendChild(renderWizard(wizards[i]));
+  // }
+  // similarListElement.appendChild(fragment);
+
+  // userDialog.querySelector('.setup-similar').classList.remove('hidden');
+})();
+
+// Файл setup.js
+(function () {
+  var MAX_SIMILAR_WIZARD_COUNT = 4;
+
+  var userDialog = document.querySelector('.setup');
+  userDialog.classList.remove('hidden');
+
+  var similarListElement = userDialog.querySelector('.setup-similar-list');
+
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
+
+  var renderWizard = function (wizard) {
+    var wizardElement = similarWizardTemplate.cloneNode(true);
+
+    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+
+    return wizardElement;
   };
-}
 
-for (var i = 0; i < 3; i++) {
-  var wizard = createRandomWizard();
-  wizards.push(wizard);
-}
+  var successHandler = function (wizards) {
+    var fragment = document.createDocumentFragment();
 
-var fragment = document.createDocumentFragment();
-for (i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizard(wizards[i]));
-}
-similarListElement.appendChild(fragment);
+    for (var i = 0; i < MAX_SIMILAR_WIZARD_COUNT; i++) {
+      fragment.appendChild(renderWizard(wizards[i]));
+    }
+    similarListElement.appendChild(fragment);
 
-userDialog.querySelector('.setup-similar').classList.remove('hidden');
+    userDialog.querySelector('.setup-similar').classList.remove('hidden');
+  };
 
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  window.backend.load(successHandler, errorHandler);
+
+})();
